040e1219af86fae7242d2c6900a7b80ebb3f3043
Anushka Singh <asingh3213@gatech.edu>
2026-04-29 23:30:38 +0100

    generators, typevar and sequence

---
+++ map.py
@@ -0,0 +1,53 @@
+from collections.abc import Callable
+from typing import Generator, Sequence, TypeVar
+
+
+class Animal:
+    def __init__(self, name: str):
+        self.name = name
+
+    def __str__(self):
+        return f"Animal(name={self.name})"
+
+
+T = TypeVar("T", bound=Animal)
+K = TypeVar("K", bound=str)
+
+
+def my_map(func: Callable[[T], K], item_list: Sequence[T]) -> Generator[K, None, None]:
+    print("Inside my_map")
+    for idx in range(len(item_list)):
+        yield func(item_list[idx])
+
+
+def square(x: int) -> int:
+    print("About to square x=", x)
+    return x**2
+
+
+def capitalize(s: str) -> str:
+    print("About to capitalize s=", s)
+    return s.capitalize()
+
+
+if __name__ == "__main__":
+    # item_ls = [1, 2, 3, 4, 5]
+    # print("Before calling my_map")
+    # map_ret = my_map(square, item_ls)
+    # print("After calling my_map")
+
+    # for i in map_ret:
+    #     print(i)
+
+    # str_ls = ["hello", "world", "python"]
+    # print("Before calling my_map for capitalize")
+    # map_ret_str = my_map(capitalize, str_ls)
+    # print("After calling my_map for capitalize")
+    # for s in map_ret_str:
+    #     print(s)
+
+    animal_ls = [Animal("cat"), Animal("dog"), Animal("rabbit")]
+
+    y = my_map(lambda a: f"Animal name: {a.name}", animal_ls)
+    for name in y:
+        print(name)
